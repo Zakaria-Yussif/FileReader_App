2deae32ca6d0cbd40f8d1e46d8224c918c19922e
ZAKARIA Zack <zackyaroo31@gmail.com>
2025-07-22 22:31:06 +0300

    data

--- FileReader/FileReader/settings.py
+++ FileReader/FileReader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vj1rud37wngvgw11%3(b12-*m3)_2a0x=!*b8x%er3dr5!s6#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'filereaderapp-production.up.railway.app', 'localhost')]
+ALLOWED_HOSTS = ['127.0.0.1', 'filereaderapp-production.up.railway.app', 'localhost']
 
 
 
